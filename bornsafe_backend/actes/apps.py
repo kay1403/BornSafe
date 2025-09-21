618d2f88def17a9fa80184b436822cd236c81575
Ange Koumba <angekoumba754@gmail.com>
2025-09-21 17:45:29 +0200

    amelioration de backend(deja operationel)

--- bornsafe_backend/actes/apps.py
+++
@@ -1,9 +0,0 @@
-# actes/apps.py
-from django.apps import AppConfig
-
-class ActesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'actes'
-
-    def ready(self):
-        import actes.signals
--- bornsafe_backend/provinces/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Province
-
-class ProvinceSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Province
-        fields = ['id', 'name']
